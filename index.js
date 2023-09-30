b8f80e42b39fca3d7150a56436860523ccca0d15
Thibault Dassise <prestataire.0193412@additi.fr>
2023-09-30 16:27:46 +0200

    refacto CSS + JS

--- index.js
+++ index.js
@@ -1,11 +1,92 @@
 
 const navbar = document.querySelector("#navbar");
-const navlink = document.querySelector(".nav__link");
+const modalSwitch = document.querySelector(".modalSwitch");
+const pricingCheckbox = modalSwitch.querySelector("input[type='checkbox']");
+const left = modalSwitch.querySelector(".left");
+const right = modalSwitch.querySelector(".right");
+const cardsButtons = document.querySelectorAll('.pricing__card button');
+let interval = document.querySelector('.pricing__card-container').dataset.interval;
+// const navlink = document.querySelector(".nav__link");
+const stripeData = {
+    monthly: {
+        standard: {
+            url: 'https://buy.stripe.com/7sIg227rFdqR9IQdQR',
+            price: '$3',
+        },
+        premium: {
+            url: 'https://buy.stripe.com/00g1784ft4Ul08g003',
+            price: '$6',
+        },
+        custom: {
+            url: 'https://checkout.stripe.com/c/pay/cs_live_a1kJqHo78Qpdppp0HiN1fPKIHWrpT26J8ENymi9ZhxpfO8m1829Na6zGcN#fidkdWxOYHwnPyd1blppbHNgWjA0S3FdUldMRGl2MjB0VnxWXDdiNURQMF1obDdzcnZWaHdMZ3dLaGprSTVwQj1WV1VAaUJIPDxudVRQUkcyQD19aUNNc0NINEdIcWNKTD1IT2ZwU0pOb3NjNTVgQjVyXUlJcScpJ3VpbGtuQH11anZgYUxhJz8nPXJIYjc3PXNPNUAwMkRMNz09J3gl',
+            price: null,
+        },
+    },
+    yearly: {
+        standard: {
+            url: 'https://buy.stripe.com/3cs8zAdQ3fyZ5sAbIK',
+            price: '$30',
+        },
+        premium: {
+            url: 'https://buy.stripe.com/cN28zA9zN4Ul3ks7sw',
+            price: '$60',
+        },
+        custom: {
+            url: 'https://checkout.stripe.com/c/pay/cs_live_a1kJqHo78Qpdppp0HiN1fPKIHWrpT26J8ENymi9ZhxpfO8m1829Na6zGcN#fidkdWxOYHwnPyd1blppbHNgWjA0S3FdUldMRGl2MjB0VnxWXDdiNURQMF1obDdzcnZWaHdMZ3dLaGprSTVwQj1WV1VAaUJIPDxudVRQUkcyQD19aUNNc0NINEdIcWNKTD1IT2ZwU0pOb3NjNTVgQjVyXUlJcScpJ3VpbGtuQH11anZgYUxhJz8nPXJIYjc3PXNPNUAwMkRMNz09J3gl',
+            price: null,
+        },
+    },
+};
+
+function handlePricingCheckbox() {
+    // initialize variables
+    let intervalText = "";
+    let buttonBackgroundColor = "";
+    // check which side is active
+    if ("monthly" === interval) {
+        // update variables values
+        intervalText = "year";
+        interval = "yearly";
+        buttonBackgroundColor = "black";
+        // update checkbox style
+        left.style.backgroundColor= "white";
+        left.style.color = "black";
+        right.style.backgroundColor = "black";
+        right.style.color = "#FFD95A";
+    } else {
+        // update variables values
+        intervalText = "month";
+        interval = "monthly";
+        buttonBackgroundColor = "#82A0D8";
+        // update checkbox style
+        left.style.backgroundColor= "#82A0D8";
+        left.style.color = "white";
+        right.style.backgroundColor = "white";
+        right.style.color = "black";
+    }
+    // loop through buttons
+    cardsButtons.forEach((button) => {
+        // get the button's card
+        const pricingCard = button.parentElement.parentElement;
+        // change the button's background color
+        button.style.backgroundColor = buttonBackgroundColor;
+        // get the link element
+        const link = button.parentElement;
+        // get the matching link from the stripeData object
+        link.href = stripeData[interval][link.parentElement.dataset.plan].url;
+        // get the plan price from stripeData object
+        const price = stripeData[interval][link.parentElement.dataset.plan].price;
+        // set card's price and interval
+        if (price) {
+            pricingCard.querySelector('h2.plan-price').textContent = price;
+            pricingCard.querySelector(".interval-text").innerHTML = `per<br>${intervalText}`;
+        }
+    });
+}
 
 window.addEventListener("scroll", () => {
 
     if (window.scrollY > 50) {
-        console.log('scroll')
         navbar.setAttribute("class", "navbar scrolled navstyle");
         // navbar.setAttribute("class", "navbar navstyle")
     } else {
@@ -14,31 +95,6 @@ window.addEventListener("scroll", () => {
     }
 });
 
-
-    const checkbox = document.getElementById("check");
-    const modalpricing = document.getElementById("pricing__card-container");
-    const modalpricingAnnual = document.getElementById("pricing__card-container-annual");
-    const left = document.getElementById("left");
-    const right = document.getElementById("right");
-
-    function verifierCaseCochée() {
-        if (checkbox.checked) {
-            modalpricing.style.display = "none"
-            modalpricingAnnual.style.display = "grid"
-            left.style.backgroundColor= "white"
-            left.style.color = "black"
-            right.style.backgroundColor = "black"
-            right.style.color = "#FFD95A"
-        } else {
-            left.style.backgroundColor= "#82A0D8"
-            left.style.color = "white"
-            modalpricing.style.display = "grid"
-            modalpricingAnnual.style.display = "none"
-            right.style.backgroundColor = "white"
-            right.style.color = "black"
-        }
-    }
-
-    checkbox.addEventListener("change", () => {
-        verifierCaseCochée()
-    })
+pricingCheckbox.addEventListener("change", () => {
+    handlePricingCheckbox();
+})
