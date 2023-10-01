2be2c3cdf71e1cf364e4d93d9895a911941e26fc
Thibault Dassise <prestataire.0193412@additi.fr>
2023-10-01 18:23:53 +0200

    web components + fix CSS

---
+++ assets/web-components/navbar.js
@@ -0,0 +1,46 @@
+class RevuflowNavbar extends HTMLElement {
+    constructor() {
+        super();
+        const shadowRoot = this.attachShadow({ mode: 'open' });
+
+        const styleSheet = document.createElement('link');
+        styleSheet.setAttribute('rel', 'stylesheet');
+        styleSheet.setAttribute('href', 'assets/main.css');
+        shadowRoot.appendChild(styleSheet);
+
+        const container = document.createElement('div');
+        container.setAttribute("class", "navbar")
+        container.innerHTML = `
+        <div class="container navbar-container">
+          <div class="navbar-holder">
+            <div class="navbar-container">
+              <a href="#intro" class="brand nav-brand">
+                <img src="assets/images/revufloLogo (2).png" class="brand-image" alt="logo">
+              </a>
+              <nav class="nav-menu">
+                <div class="nav-links-holder">
+                  <div class="nav-links-container">
+                    <div class="nav-links">
+                      <a href="#offerings" class="nav-link">Offerings</a>
+                      <a href="#the-product" class="nav-link">The product</a>
+                      <a href="#pricing" class="nav-link">Pricing</a>
+                      <a href="#contact-us" class="nav-link">Contact Us</a>
+                    </div>
+                    <div class="nav-button-holder">
+                      <a href="#pricing" class="navbar-button">Start now</a>
+                    </div>
+                  </div>
+                </div>
+              </nav>
+              <button class="nav-button">
+                <img src="assets/images/hamburger-icon.png" width="10%" class="nav-icon" alt="hamburger icon">
+              </button>
+            </div>
+          </div>
+        </div>
+        `;
+        shadowRoot.appendChild(container);
+    }
+}
+
+customElements.define('revuflow-navbar', RevuflowNavbar);
